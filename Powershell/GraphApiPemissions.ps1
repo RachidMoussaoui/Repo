86eeaca0d03ec1092b0da9445fe207f4df529d91
RachidMoussaoui <rachid_elmoussaoui@cloudkoning.com>
2025-01-25 09:39:45 +0100

    Update GraphApiPemissions.ps1

--- Powershell/GraphApiPemissions.ps1
+++ Powershell/GraphApiPemissions.ps1
@@ -2,64 +2,8 @@
 $ObjectId = "91a627b3-9401-4dc1-bed9-6412a4aff791"
 
 # Define the Graph scopes to grant
-$graphScopes = @(
-    "Organization.Read.All"
-    "AccessReview.Read.All"
-    "Policy.Read.All"
-    "AdministrativeUnit.Read.All"
-    "RoleManagement.Read.Directory"
-    "Application.Read.All"
-    "CustomSecAttributeDefinition.Read.All"
-    "Policy.Read.ConditionalAccess"
-    "Policy.ReadWrite.AuthenticationMethod"
-    "UserAuthenticationMethod.Read.All"
-    "Agreement.Read.All"
-    "Group.Read.All"
-    "User.Read.All"
-    "Directory.Read.All"
-    "Policy.Read.DeviceConfiguration"
-    "Domain.Read.All"
-    "NetworkAccess.Read.All"
-    "EntitlementManagement.Read.All"
-    "ReportSettings.Read.All"
-    "IdentityUserFlow.Read.All"
-    "LifecycleWorkflows.Read.All"
-    "ProgramControl.Read.All"
-    "Policy.Read.IdentityProtection"
-    "NetworkAccessPolicy.Read.All"
-    "RoleAssignmentSchedule.Read.Directory"
-    "RoleEligibilitySchedule.Read.Directory"
-    "RoleManagementPolicy.Read.Directory"
-    "RoleManagement.Read.All"
-    "IdentityProvider.Read.All"
-    "DeviceManagementConfiguration.Read.All"
-    "DeviceManagementApps.Read.All"
-    "DeviceManagementManagedDevices.Read.All"
-    "DeviceManagementConfiguration.ReadWrite.All"
-    "DeviceManagementServiceConfig.Read.All"
-    "DeviceManagementRBAC.Read.All"
-    "ExternalConnection.Read.All"
-    "Application.ReadWrite.All"
-    "OrgSettings-Microsoft365Install.Read.All"
-    "OrgSettings-Forms.Read.All"
-    "OrgSettings-Todo.Read.All"
-    "OrgSettings-AppsAndServices.Read.All"
-    "OrgSettings-DynamicsVoice.Read.All"
-    "Tasks.Read.All"
-    "SharePointTenantSettings.Read.All"
-    "User.Read.All"
-    "Group.ReadWrite.All"
-    "AppCatalog.ReadWrite.All"
-    "TeamSettings.ReadWrite.All"
-    "Channel.Delete.All"
-    "ChannelSettings.ReadWrite.All"
-    "ChannelMember.ReadWrite.All"
-    "ChannelSettings.Read.All"
-   
-
-
-  
-)
+$graphScopes = @("ADDyourGraphScpopes")
+   )
 
 # Connect to Microsoft Graph with the required permissions
 Connect-MgGraph 
